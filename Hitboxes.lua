040ecb926ed2f98cd978c4345ae8d7aa8fdc9e5e
0oferApple <79620058+ForbiddenApple@users.noreply.github.com>
2022-10-21 21:22:06 -0700

    Create Hitboxes.lua

---
+++ Hitboxes.lua
@@ -0,0 +1,181 @@
+local hitboxes = {
+        -- Reimu/Sanae
+        ["GoheiHB"] = true,
+        ["GoheiLast"] = true,
+        ["GoheiHeavy"] = true,
+        
+        -- Reimu
+        ["bindghb"] = true,
+        ["bindairhb"] = true,
+        ["CounterProj"] = true,
+        
+        -- Marisa
+        ["BroomHB"] = true,
+        ["BroomLast"] = true,
+        ["BroomHeavy"] = true,
+        ["Bombhb"] = true,
+        
+        -- Sanae
+        ["Wavehb"] = true,
+        ["ringhb"] = true,
+        ["Onbashirahb"] = true,
+        ["moseshb"] = true,
+        ["ritualhb"] = true,
+        
+        -- Sakuya
+        ["KnifeHB"] = true,
+        ["KnifeLast"] = true,
+        ["KnifeHeavy"] = true,
+        ["propellinghb"] = true,
+        ["chainsawhb"] = true,
+        ["knifehb"] = true,
+        ["inscribedhb"] = true,
+        ["newscribedhb"] = true,
+        ["newscribedlasthb"] = true,
+        
+        -- Meiling
+        ["FistHB"] = true,
+        ["Fist2HB"] = true,
+        ["FistLast"] = true,
+        ["FistHeavy"] = true,
+        ["fragranthb"] = true,
+        ["colorfulhb"] = true,
+        ["starproj"] = true,
+        ["RKFhb"] = true,
+        ["raginghb"] = true,
+        
+        -- Youmu
+        ["SwordHB"] = true,
+        ["SwordHB2"] = true,
+        ["SwordLast"] = true,
+        ["SwordLast2"] = true,
+        ["SwordHeavy"] = true,
+        ["m2rangehb"] = true,
+        ["crescenthb"] = true,
+        ["risehb"] = true,
+        ["insightfulslash"] = true,
+        ["intoleranthb"] = true,
+        
+        -- Mokou
+        ["MokouHB"] = true,
+        ["MokouLast"] = true,
+        ["MokouBreakerHB"] = true,
+        ["MokouBreakerLast"] = true,
+        ["MokouHeavy"] = true,
+        ["bindairhb"] = true,
+        ["flyingproj"] = true,
+        ["tornadohb"] = true,
+        ["huntinghb"] = true,
+        
+        -- Kokoro
+        ["KokoHB"] = true,
+        ["KokoHB2"] = true,
+        ["KokoLast"] = true,
+        ["KokoM2"] = true,
+        ["joyhb"] = true,
+        ["angryHB"] = true,
+        ["angryproj"] = true,
+        ["fearHB"] = true,
+        ["resolvehb"] = true,
+        ["delightedhb"] = true,
+        ["worriesHB"] = true,
+        
+        -- Yukari
+        ["FanHB"] = true,
+        ["FanLast"] = true,
+        ["YukariM2"] = true,
+        ["sign1hb"] = true,
+        ["nesthb"] = true,
+        ["nesthb2"] = true,
+        ["YukaTrain"] = true,
+        ["lendlegshb"] = true,
+        
+        -- Alice
+        ["SpearHB"] = true,
+        ["DollSpinHB"] = true,
+        ["ArrowHB"] = true,
+        ["PierceHB"] = true,
+        
+        -- Komachi
+        ["ScytheHB"] = true,
+        ["ScytheLast"] = true,
+        ["ScytheHeavy"] = true,
+        ["reapHB1"] = true,
+        ["reapHB2"] = true,
+        ["exorcismhb1"] = true,
+        ["exorcismhb2"] = true,
+        ["assimilatehb"] = true,
+        ["endlesshb"] = true,
+        
+        -- Okuu
+        ["RodHB"] = true,
+        ["RodHB2"] = true,
+        ["RodLast"] = true,
+        ["fusionstarhb"] = true,
+        ["radianthb"] = true,
+        ["meltinghb"] = true,
+        ["rockethb"] = true,
+        
+        -- Rumia
+        ["RumiaHB"] = true,
+        ["RumiaLast"] = true,
+        ["RumiaHeavy"] = true,
+        ["nbhb"] = true,
+        ["nbhb2"] = true,
+        ["mlhb"] = true,
+        ["darksidehb"] = true,
+        
+        -- Cirno
+        ["CirnoHB"] = true,
+        ["CirnoHB3"] = true,
+        ["CirnoLast"] = true,
+        ["CirnoHeavy"] = true,
+        ["GsHB"] = true,
+        ["GsLast"] = true,
+        ["GsHeavy"] = true,
+        ["RapierHB"] = true,
+        ["RapierLast"] = true,
+        ["icelasthb"] = true,
+        ["iciclehb"] = true,
+        ["slidehb"] = true,
+        ["frozentechhb"] = true,
+        ["lancehb"] = true,
+        ["kickhb"] = true,
+        ["stridehb"] = true,
+        ["piercerhb"] = true,
+        ["frostpillarshb"] = true,
+        ["eleganthb"] = true,
+        
+        -- Suika
+        ["SuikaHB"] = true,
+        ["SuikaLast"] = true,
+        ["armslamhb"] = true,
+        ["gatheringhb"] = true,
+        ["AtlasProj"] = true,
+        ["bindinghb"] = true,
+        ["stephb"] = true,
+        
+        -- Tenshi
+        ["TenshiHB"] = true,
+        ["TenshiLast"] = true,
+        ["TenshiHeavy"] = true,
+        ["m2hb"] = true,
+        ["PerceptionHB"] = true,
+        ["revhb"] = true,
+        ["rapturehb"] = true,
+        ["skyhb"] = true,
+        ["unlettinghb"] = true,
+        ["unlettinghb2"] = true,
+        
+        -- Aya
+        ["AyaHB"] = true,
+        ["AyaLast"] = true,
+        ["AyaHB"] = true,
+        ["AyaHB"] = true,
+        ["domhb"] = true,
+        ["downbursthb"] = true,
+        ["guideancehb"] = true,
+        ["maplehb"] = true,
+        ["illuhb"] = true,
+        ["galehb"] = true,
+    }
